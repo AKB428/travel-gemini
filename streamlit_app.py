e1c63408dc83c94867eb08451f0f9993bfe381e2
AKB428 <gandara016+1@gmail.com>
2024-10-27 13:12:15 +0900

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ prefectures = [
 
 # Streamlit UI
 st.title("日本旅行モデルコース生成アプリ")
-st.write("以下のパラメーターを選択して、旅行プランを生成してください。")
+st.write("旅行プランを生成してください。生成された旅行プランは生成AIで作成されており誤った情報が含まれている可能性があるため、あくまで参考として事実確認は自身で行うようにお願いします。")
 
 # ユーザー入力
 start = st.selectbox("出発する都道府県", prefectures, index=prefectures.index("東京"))
